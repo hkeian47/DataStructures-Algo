5d572f883f8cae1ddbd8df7a7c65e80aab00429f
Hana <hanakeinan11@gmail.com>
2022-03-25 11:24:30 -0500

    Initial commit

--- src/binarysearchtrees/BinarySearchTree.java
+++ src/binarysearchtrees/BinarySearchTree.java
@@ -1,2 +1,15 @@
-package binarysearchtrees;public class BinarySearchTree {
+package binarysearchtrees;
+
+/**
+ * The type Binary search tree.
+ */
+public class BinarySearchTree {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(" THIS THE ONE THAT I NEED");
+    }
 }
---
+++ src/datastructures/Driver.java
@@ -0,0 +1,15 @@
+package datastructures;
+
+/**
+ * The type Driver.
+ */
+public class Driver {
+     /**
+      * The entry point of application.
+      *
+      * @param args the input arguments
+      */
+     public static void main(String[] args) {
+          System.out.println(" I'm creating this another method");
+     }
+}
